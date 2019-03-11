c41374413b991c6982ee4dc3a7bd08984ab691b4
tlglovewf <tuligen@navinfo.com>
2019-03-11 15:10:00 +0800

    init

---
+++ cmake_module/PangolinConfig.cmake
@@ -0,0 +1,7 @@
+# Compute paths
+SET( Pangolin_INCLUDE_DIRS "/Volumes/mac/OriginSource/ORB-SLAM2/Thirdparty/Pangolin/include;/Volumes/mac/OriginSource/ORB-SLAM2/Thirdparty/Pangolin/build/src/include;/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk/System/Library/Frameworks/OpenGL.framework;/opt/local/include;/usr/local/include/eigen3" )
+SET( Pangolin_INCLUDE_DIR  "/Volumes/mac/OriginSource/ORB-SLAM2/Thirdparty/Pangolin/include;/Volumes/mac/OriginSource/ORB-SLAM2/Thirdparty/Pangolin/build/src/include;/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk/System/Library/Frameworks/OpenGL.framework;/opt/local/include;/usr/local/include/eigen3" )
+
+SET( Pangolin_LIBRARIES    pangolin )
+SET( Pangolin_LIBRARY      pangolin )
+
